57af8573d3743a621067c8a7f74bda2455cc2fb5
John Speck <speckworks@gmail.com>
2019-08-29 12:36:23 -0400

    Animal Zoo Lab Start

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,23 @@
 class Animal
+    attr_reader :species, :nickname 
+    attr_accessor :weight, :name
+    @@all = []
 
+    def initialize (species, weight, nickname, zoo) #name signifies zoo name
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @name = zoo
+        @@all << self
+    end
+
+    def self.all
+        return @@all
+    end
+
+    def zoo
+       Zoo.all.select do |zoo_singular|
+        Zoo.name == self
+       end
+    end
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,15 @@
 class Zoo
+    attr_reader :name, :location
+    @@all = []
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def self.all
+        return @@all
+    end
+
 
 end
